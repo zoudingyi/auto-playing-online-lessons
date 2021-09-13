7469ad097431b28d3638ad30401fe62c80e5ce44
zoudingyi <zoudingyi@qq.com>
2021-09-13 11:09:38 +0800

    Initialization Settings

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   userName: '',
   passWord: '',
   loginPage: 'http://cce.scu.edu.cn/mana/view/home/loginPlatform.do?type=1',
-  lesson : 2, // 第几课
-  chapter: 15, // 第几章
-  section: 5 // 第几小节
+  lesson : 1, // 第几课
+  chapter: 1, // 第几章
+  section: 1 // 第几小节
 }
